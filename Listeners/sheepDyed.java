dda95ac4cd74c346ad6de88972cebf1629b637de
FLOWOGithub <108979847+FLOWOGithub@users.noreply.github.com>
2022-07-09 14:29:28 +0300

    Add files via upload

--- Listeners/sheepDyed.java
+++ Listeners/sheepDyed.java
@@ -8,22 +8,22 @@ import org.bukkit.event.entity.SheepDyeWoolEvent;
 
 public class sheepDyed implements Listener {
     @EventHandler
-    public void onSheepDyed(SheepDyeWoolEvent e){
+    public void onSheepDyed(SheepDyeWoolEvent e) {
         Player p = e.getPlayer();
-        if (p.getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == 3 || p.getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == 4){
-            e.setCancelled(true);
-        } else if ( p.getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == 1) {
-            e.setCancelled(true);
+        if (p.getInventory().getItemInMainHand().getItemMeta().hasCustomModelData()) {
+            if (p.getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == 3 || p.getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == 4) {
+                e.setCancelled(true);
+            } else if (p.getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == 1) {
+                e.setCancelled(true);
 
-            if(p.getMaxHealth()<40) {
-                p.getItemInHand().setAmount(p.getItemInHand().getAmount()-1);
-                p.setMaxHealth(p.getMaxHealth()+2);
-            }else {
-                p.sendMessage(ChatColor.RED + "Tu jau pasiekiai 20 širdučių.");
+                if (p.getMaxHealth() < 40) {
+                    p.getItemInHand().setAmount(p.getItemInHand().getAmount() - 1);
+                    p.setMaxHealth(p.getMaxHealth() + 2);
+                } else {
+                    p.sendMessage(ChatColor.RED + "Tu jau pasiekiai 20 širdučių.");
+                }
+
             }
-
-        }else{
-            return;
         }
     }
 }
